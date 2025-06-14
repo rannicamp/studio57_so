49a720dcb75907beeb1e1c67593051b19a017586
rannicamp <rannierecampos1@gmail.com>
2025-06-14 16:24:26 -0300

    atualização novo projeto supabase

--- public/js/supabase-config.js
+++ public/js/supabase-config.js
@@ -3,7 +3,7 @@
 // Suas chaves de acesso ao Supabase.
 // É seguro deixá-las aqui, pois o Supabase tem a segurança a nível de linha (RLS).
 const supabaseUrl = 'https://vhuvnutzklhskkwbpxdz.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZodXZudXR6a2xoc2trd2JweGR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MjY2NDYsImV4cCI6MjA2NTUwMjY2NH0.VjQI3_Oy3KWWAoYF3KVB00XHeiUP5BfTxNlaC50UXeU';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZodXZudXR6a2xoc2trd2JweGR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MjY2NDYsImV4cCI6MjA2NTUwMjY0Nn0.VjQI3_Oy3KWWAoYF3KVB00XHeiUP5BfTxNlaC50UXeU';
 
 // Cria e exporta o cliente Supabase para ser usado em todo o sistema
 const { createClient } = supabase;
